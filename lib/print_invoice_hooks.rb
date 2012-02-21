37f1a38ed3e5df3bc802a1a3c44af938954097b8
Tim Case <tim@poweupdev.com>
2012-02-21 02:26:43 -0200

    adding print buttons

--- lib/print_invoice_hooks.rb
+++ lib/print_invoice_hooks.rb
@@ -1,8 +1,16 @@
 class PrintInvoiceHooks < Spree::ThemeSupport::HookListener
+  
+  Deface::Override.new(:virtual_path => "spree/admin/orders/show",
+  :name => "print_buttons",
+  :insert_after => "[data-hook='admin_order_show_buttons'], #admin_order_show_buttons[data-hook]",
+  :partial => "admin/orders/print_buttons",
+  :disabled => false)
 
+  Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
+  :name => "print_buttons",
+  :insert_after => "[data-hook='admin_order_edit_buttons'], #admin_order_edit_buttons[data-hook]",
+  :partial => "admin/orders/print_buttons",
+  :disabled => false)
 
-  insert_after :admin_order_show_buttons , 'print_buttons'
-
-  insert_after :admin_order_edit_buttons , 'print_buttons'
 
 end
