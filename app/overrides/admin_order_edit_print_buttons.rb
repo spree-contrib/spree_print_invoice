ddea6a1a25cbb391ff53ab26594d4280e3694edb
Tim Case <tim@poweupdev.com>
2012-02-21 13:19:12 -0200

    add spree prefix

--- app/overrides/admin_order_edit_print_buttons.rb
+++ app/overrides/admin_order_edit_print_buttons.rb
@@ -2,5 +2,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
 :name => "print_buttons",
 :insert_after => "[data-hook='admin_order_edit_buttons'], #admin_order_edit_buttons[data-hook]",
-:partial => "admin/orders/print_buttons",
+:partial => "spree/admin/orders/print_buttons",
 :disabled => false)
