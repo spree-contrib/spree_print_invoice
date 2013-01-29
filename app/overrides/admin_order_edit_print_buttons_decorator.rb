b51a73818536ea3678c1c76f4862e4beb1a593dd
Ted Lilley <tlilley@diditbetter.com>
2013-01-29 17:14:30 +0000

    Update for Spree 1.3

--- app/overrides/admin_order_edit_print_buttons_decorator.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
-                     :name => "print_buttons",
-                     :insert_bottom => "[data-hook='admin_order_edit_buttons'], #admin_order_edit_buttons[data-hook]",
-                     :partial => "spree/admin/orders/print_buttons",
-                     :disabled => false)
