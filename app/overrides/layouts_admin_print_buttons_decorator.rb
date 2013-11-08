84223d36a3f8e050b5923f842889b66369afff0c
Ryan Bigg <radarlistener@gmail.com>
2013-11-08 15:03:32 +1100

    Correct virtual_path for print buttons override
    Fixes #34

--- app/overrides/layouts_admin_print_buttons_decorator.rb
+++ app/overrides/layouts_admin_print_buttons_decorator.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_content_header",
                      :name => "print_buttons",
                      :insert_top => "[data-hook='toolbar']>ul",
                      :partial => "spree/admin/orders/print_buttons",
