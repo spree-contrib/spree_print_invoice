9188b424ee5e60eddbf2648aa75ec8a8d6e7d065
Michael Lippold <michael.lippold@smartacus.net>
2014-03-21 22:37:59 -0700

    Add PDF rendering options - Add basic font configuration to General Settings > Invoices Settings - Added configuration setting to easily scale logo image
    Fixes #47

---
+++ app/helpers/spree/admin/print_invoice_helper.rb
@@ -0,0 +1,14 @@
+module Spree
+  module Admin
+    module PrintInvoiceHelper
+      def font_faces
+        fonts = Prawn::Font::AFM::BUILT_INS.reject { |f| f =~ /zapf|symbol|bold|italic|oblique/i }.map { |f| [f.tr('-', ' '), f] }
+        options_for_select(fonts, Spree::PrintInvoice::Config[:print_invoice_font_face])
+      end
+
+      def logo_scale
+        Spree::PrintInvoice::Config[:print_invoice_logo_scale].to_f / 100
+      end
+    end
+  end
+end
