c49269937bac93bfdbbc046d361c5c90b1b3d212
manu <ssen.dev+mbmskyline@gmail.com>
2014-04-03 19:40:30 +0200

    added config option for prawn (allows setting page size, margins)
    Fixes #49

--- app/models/spree/print_invoice_configuration.rb
+++ app/models/spree/print_invoice_configuration.rb
@@ -4,6 +4,7 @@ module Spree
     preference :print_invoice_next_number, :integer, :default => nil
     preference :print_invoice_logo_path, :string, :default => Spree::Config[:admin_interface_logo]
     preference :print_buttons, :string, :default => 'invoice'
+    preference :prawn_options, :hash, :default => {}
 
     def use_sequential_number?
       print_invoice_next_number.present? && print_invoice_next_number > 0
