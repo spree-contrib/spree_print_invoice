94c530baa087b1c14cf7fa50cdfdcaa30f778b60
Tobias Bohwalli <hi@futhr.io>
2014-12-08 18:06:03 +0100

    Move print buttons into order tabs.
    Filling title space with buttons will break layout quick. Using
    Glyphicon ‘save-file’ would look nicer but current Spree version of
    Bootstrap do not have that one.

--- app/overrides/layouts_admin_print_buttons_decorator.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_content_header',
-                     name:         'print_buttons',
-                     insert_top:   '[data-hook="toolbar"]>ul',
-                     partial:      'spree/admin/orders/print_buttons',
-                     disabled:     false)
--- spec/features/spree/admin/print_invoice_spec.rb
+++ spec/features/spree/admin/print_invoice_spec.rb
@@ -1,22 +1,6 @@
 RSpec.feature 'Admin Print Invoice', :js do
   stub_authorization!
 
-  context 'can not print' do
-
-    background do
-      @order = create(:order_with_line_items)
-      visit spree.admin_orders_path
-    end
-
-    scenario 'when no completed order exist' do
-      uncheck 'Only show complete orders'
-      click_button 'Filter Results'
-      expect(page).to have_text @order.user.email
-      within_table('listing_orders') { click_icon :edit }
-      expect(page).not_to have_link 'Print Invoice'
-    end
-  end
-
   context 'can print' do
 
     background do
@@ -26,22 +10,15 @@ RSpec.feature 'Admin Print Invoice', :js do
 
     scenario 'completed order' do
       expect(@order.completed_at).not_to be_nil
-      expect(@order.state).to eq 'complete'
 
-      uncheck 'Only show complete orders'
-      expect(page).to have_text @order.user.email
-      within_table('listing_orders') { click_icon :edit }
-
-      expect(page).to have_link 'Print Invoice'
+      within_table('listing_orders') do
+        expect(page).to have_text @order.user.email
+        click_icon :edit
+      end
+      within('#sidebar') do
+        expect(page).to have_link 'Print Invoice'
+        expect(page).to have_link 'Print Packaging Slip'
+      end
     end
-  end
-
-  private
-
-  def show_invoice_pdf_for(order)
-    click_link 'Print Invoice'
-    expect(current_path).to eq "/admin/orders/#{order.number}.pdf?template=invoice"
-    expect(page.response_headers['Content-Type']).to eq 'application/pdf'
-    expect(page.response_headers['Content-Disposition']).to eq "attachment; filename=#{order.number}.pdf"
   end
 end
