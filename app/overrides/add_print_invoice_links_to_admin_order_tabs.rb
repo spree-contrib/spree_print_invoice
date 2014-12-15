ed0644a3c948568a1e20ac67d867b8a98aa42c08
Tobias Bohwalli <hi@futhr.io>
2014-12-15 23:45:23 +0100

    Move slips to bottom in list.

--- app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
+++ app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path: 'spree/admin/shared/_order_tabs',
                      name:         'print_invoice_order_tab_links',
-                     insert_top:   '[data-hook="admin_order_tabs"]',
+                     insert_bottom: '[data-hook="admin_order_tabs"]',
                      partial:      'spree/admin/orders/print_invoice_order_tab_links',
                      disabled:     false)
