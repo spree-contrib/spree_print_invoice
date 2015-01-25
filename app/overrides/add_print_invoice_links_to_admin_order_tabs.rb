6f7c67bda439b04bb83d275456dd431c76d33367
Tobias Bohwalli <hi@futhr.io>
2015-01-25 11:03:00 +0100

    Remove Deface option "disabled".
    Disabled option is unnecessary and may just raise deprecation warnings.

--- app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
+++ app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_order_tabs',
-                     name:         'print_invoice_order_tab_links',
-                     insert_bottom: '[data-hook="admin_order_tabs"]',
-                     partial:      'spree/admin/orders/print_invoice_order_tab_links',
-                     disabled:     false)
+Deface::Override.new(
+  virtual_path: 'spree/admin/shared/_order_tabs',
+  name:         'print_invoice_order_tab_links',
+  insert_bottom: '[data-hook="admin_order_tabs"]',
+  partial:      'spree/admin/orders/print_invoice_order_tab_links'
+)
