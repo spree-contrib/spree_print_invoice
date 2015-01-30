7dded206aea1500c25a431b5adafb37790ae4384
Tobias Bohwalli <hi@futhr.io>
2015-01-30 17:22:00 +0100

    Remove dup strategy.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,7 +2,6 @@ require 'database_cleaner'
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
 
