91596b3f98b110bab97883553ad4f0c07fa3bc27
Roman Smirnov <POMAHC@gmail.com>
2015-03-03 12:49:32 +0300

    Fixes admin/orders/show route. Fixes #71

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Spree::Core::Engine.add_routes do
   namespace :admin do
-    resources :orders do
-      member do
-        get :show
-      end
-    end
+    # Add orders show-route to routes defined in the Spree Core
+    # https://github.com/spree/spree/blob/3-0-stable/backend/config/routes.rb#L73
+    resources :orders
     resource :print_invoice_settings, only: [:edit, :update]
   end
 end
