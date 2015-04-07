4c586d7682c5919593a15b17f6e668ff1e10ec02
Thomas von Deyen <tvd@magiclabs.de>
2015-04-07 16:09:19 +0200

    Add javascript injection to install generator.

--- lib/generators/spree_print_invoice/install/install_generator.rb
+++ lib/generators/spree_print_invoice/install/install_generator.rb
@@ -3,6 +3,10 @@ module SpreePrintInvoice
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, type: :boolean, default: true
 
+      def add_javascripts
+        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_print_invoice\n"
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_print_invoice'
       end
