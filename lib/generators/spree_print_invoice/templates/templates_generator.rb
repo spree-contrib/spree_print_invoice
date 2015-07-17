c531bd710bdb3cd30b2fbd3fa44bb7b7223ef0a4
Martin Meyerhoff <mamhoff@gmail.com>
2015-07-17 15:15:30 +0200

    Adapt copy template generator
    Now just copies the entire `app/views/spree/printables` directory.

--- lib/generators/spree_print_invoice/templates/templates_generator.rb
+++ lib/generators/spree_print_invoice/templates/templates_generator.rb
@@ -1,11 +1,10 @@
 module SpreePrintInvoice
   module Generators
     class TemplatesGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../../../../app/views/spree/admin/orders", __FILE__)
+      source_root File.expand_path("../../../../../app/views/spree", __FILE__)
 
       def copy_templates
-        copy_file "invoice.pdf.prawn", "app/views/spree/admin/orders/invoice.pdf.prawn"
-        copy_file "packaging_slip.pdf.prawn", "app/views/spree/admin/orders/packaging_slip.pdf.prawn"
+        directory "printables", "app/views/spree/printables"
       end
     end
   end
