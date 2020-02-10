cc102540ce1a317ff362b36bb705e97bbe14dfe3
Ivan Sheremeta <iisheremeta@gmail.com>
2020-02-10 06:22:50 +0200

    remove class eval from order

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,43 +1,48 @@
-Spree::Order.class_eval do
-  has_many :bookkeeping_documents, as: :printable, dependent: :destroy
-  has_one :invoice, -> { where(template: 'invoice') },
-          class_name: 'Spree::BookkeepingDocument',
-          as: :printable
-  has_one :packaging_slip, -> { where(template: 'packaging_slip') },
-          class_name: 'Spree::BookkeepingDocument',
-          as: :printable
+# frozen_string_literal: true
 
-  delegate :number, :date, to: :invoice, prefix: true
+module Spree
+  # Override default Spree Order
+  class Order
+    has_many :bookkeeping_documents, as: :printable, dependent: :destroy
+    has_one :invoice, -> { where(template: 'invoice') },
+            class_name: 'Spree::BookkeepingDocument',
+            as: :printable
+    has_one :packaging_slip, -> { where(template: 'packaging_slip') },
+            class_name: 'Spree::BookkeepingDocument',
+            as: :printable
 
-  # Create a new invoice before transitioning to complete
-  #
-  state_machine.before_transition to: :complete, do: :invoice_for_order
+    delegate :number, :date, to: :invoice, prefix: true
 
-  # Backwards compatibility stuff. Please don't use these methods, rather use the
-  # ones on Spree::BookkeepingDocument
-  #
-  def pdf_file
-    ActiveSupport::Deprecation.warn('This API has changed: Please use order.invoice.pdf instead')
-    invoice.pdf
-  end
+    # Create a new invoice before transitioning to complete
+    #
+    state_machine.before_transition to: :complete, do: :invoice_for_order
 
-  def pdf_filename
-    ActiveSupport::Deprecation.warn('This API has changed: Please use order.invoice.file_name instead')
-    invoice.file_name
-  end
+    # Backwards compatibility stuff. Please don't use these methods, rather use the
+    # ones on Spree::BookkeepingDocument
+    #
+    def pdf_file
+      ActiveSupport::Deprecation.warn('This API has changed: Please use order.invoice.pdf instead')
+      invoice.pdf
+    end
 
-  def pdf_file_path
-    ActiveSupport::Deprecation.warn('This API has changed: Please use order.invoice.pdf_file_path instead')
-    invoice.pdf_file_path
-  end
+    def pdf_filename
+      ActiveSupport::Deprecation.warn('This API has changed: Please use order.invoice.file_name instead')
+      invoice.file_name
+    end
 
-  def pdf_storage_path(template)
-    ActiveSupport::Deprecation.warn('This API has changed: Please use order.{packaging_slip, invoice}.pdf_file_path instead')
-    bookkeeping_documents.find_by!(template: template).file_path
-  end
+    def pdf_file_path
+      ActiveSupport::Deprecation.warn('This API has changed: Please use order.invoice.pdf_file_path instead')
+      invoice.pdf_file_path
+    end
 
-  def invoice_for_order
-    bookkeeping_documents.create(template: 'invoice')
-    bookkeeping_documents.create(template: 'packaging_slip')
+    def pdf_storage_path(template)
+      ActiveSupport::Deprecation.warn('This API has changed: Please use order.{packaging_slip, invoice}.pdf_file_path instead')
+      bookkeeping_documents.find_by!(template: template).file_path
+    end
+
+    def invoice_for_order
+      bookkeeping_documents.create(template: 'invoice')
+      bookkeeping_documents.create(template: 'packaging_slip')
+    end
   end
 end
