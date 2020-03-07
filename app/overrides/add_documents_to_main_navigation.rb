9adfc2a8500fd09d53fb15572db7e262adef24e2
Gary Lai <garylai1990@gmail.com>
2020-03-06 18:46:12 -0800

    insert to correct position

--- app/overrides/add_documents_to_main_navigation.rb
+++ app/overrides/add_documents_to_main_navigation.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path:  'spree/layouts/admin',
-  insert_bottom: '#main-sidebar',
+  insert_bottom: '#main-sidebar nav',
   partial:       'spree/admin/shared/menu/documents_tab',
   name:          'documents_tab'
 )
