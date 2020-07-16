1400cf0d813739c247d3a6f27e610f1070b737d9
Gary Lai <garylai1990@gmail.com>
2020-07-16 09:19:07 +0800

    chore(lint): lint

--- app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
+++ app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_order_tabs',
-  name:         'print_invoice_order_tab_links',
+  name: 'print_invoice_order_tab_links',
   insert_bottom: '[data-hook="admin_order_tabs"]',
-  partial:      'spree/admin/orders/print_invoice_order_tab_links'
+  partial: 'spree/admin/orders/print_invoice_order_tab_links'
 )
